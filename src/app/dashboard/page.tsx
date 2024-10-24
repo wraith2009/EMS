db54681cb677b692c894240f3de7717882244449
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-10-24 13:34:48 +0530

    Refactor teacher action to fetch teacher data by user ID and handle error cases

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import TeacherDashboard from "@/src/components/dashboard/teacherDashboard/page";
+import React from "react";
 const Dashboard = () => {
   return (
     <div className="bg-[#f3f7f9]">
